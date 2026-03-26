de37f5ad88889ad4ddcc3327a2d0dcacc52f762b
Mohamad Ghashim <mohamad.ghashim@ogr.sakarya.edu.tr>
2026-03-26 17:19:39 +0300

    Force HTTPS for assets loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL; 
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -17,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
     {
-        //
+        // إجبار الروابط على استخدام HTTPS إذا كنا على سيرفر Render
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
